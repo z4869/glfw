b54a6659e0cf55e6e612d11e260ffa09ae49522a
nest_4869 <38480945+z4869@users.noreply.github.com>
2019-03-25 08:25:27 +0900

    edit premake file

--- premake5.lua
+++ premake5.lua
@@ -19,28 +19,28 @@ project "GLFW"
     }
 
     filter "system:windows"
-    buildoptions { "-std=c11","-lgdi32" }
-    systemversion "latest"
-    staticruntime "On"
+        buildoptions { "-std=c11","-lgdi32" }
+        systemversion "latest"
+        staticruntime "On"
 
-    files
-    {
-        "src/win32_init.c",
-        "src/win32_joystick.c",
-        "src/win32_monitor.c",
-        "src/win32_time.c",
-        "src/win32_thread.c",
-        "src/win32_window.c",
-        "src/wgl_context.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c"
-    }
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
 
-    defines
-    {
-        "_GLFW_WIN32",
-        "_CRT_SECURE_NO_WARNINGS"
-    }
+        defines
+        {
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
 
     filter { "system:windows","configurations:Release" }
-    buildoptions "/MT"
+        buildoptions "/MT"
